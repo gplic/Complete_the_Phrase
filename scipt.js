36553203d57884c3975ee8392ba8c9dad18c50e9
gplic <145370247+gplic@users.noreply.github.com>
2023-11-20 09:59:58 -0700

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,31 @@
+// Compleete the phrase
+
+//a blessing in disguise
+document.getElementById("devil").addEventListener("click", devil);
+function devil() {
+    alert("devil.");
+}
+
+//a dime a dozen
+document.getElementById("leg").addEventListener("click", leg);
+function leg() {
+    alert("leg.");
+}
+
+//better late than never
+document.getElementById("corners").addEventListener("click", corners);
+function corners() {
+    alert("corners.");
+}
+
+//no pain no gain
+document.getElementById("draw").addEventListener("click", draw);
+function draw() {
+    alert("drawing board.");
+}
+
+//time flies when your having fun
+document.getElementById("both").addEventListener("click", both);
+function both() {
+    alert("worlds.");
+}
